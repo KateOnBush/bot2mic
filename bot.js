c9c62c4bf031d3dcb1ee09ac63a33784c9ad90f9
DarkZYT <aouabsss@gmail.com>
2018-04-17 16:11:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var commandPrefix = "_";
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === "'ping") {
-    	message.channel.send(':ping_pong: pong!');
+    if (message.content === commandPrefix + "avatar") {
+    	message.reply("Here's your avatar ! " + message.author.avatarURL);
   	}
 });
-
 client.on('message', message => {
-    if (message.content === 'bing') {
+    if (message.toString().startsWith(commandPrefix + "setcommandprefix")) {
+        if (message.toString().replace(commandPrefix + "setcommandprefix ").length !== 1)
+        {
+    	    message.channel.send(":x: Error while trying to set the command prefix. Please try again.");
+        }
+        else
+        {
+            commandPrefix = message.toString().replace(commandPrefix + "setcommandprefix ");
+            message.channel.send(":white_check_mark: Command Prefix Successfully set to : " + commandPrefix)
+        }
+  	}
+});
+client.on('message', message => {
+    if (message.content === commandPrefix + 'bing') {
     	message.reply('BONG!');
   	}
 });
