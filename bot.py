950fc74209dcd46e8be3cda5965242b95551f7ac
DarkZYT <aouabsss@gmail.com>
2018-04-17 15:31:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def ping(*args):
 
 # After you have modified the code, feel free to delete the line above so it does not keep popping up everytime you initiate the ping commmand.
 	
-client.run('NDM1NTc2NTIwMTkxMjQ2MzM4.DbeNGA.LET8JRsCP3kYV4NIrtDy7kUVwLE')
+client.run('NDM1NTc2NTIwMTkxMjQ2MzM4.DbeWrA.vtWuc_ukaNEzsTmMXUzDLnFgnwA')
 
 # Basic Bot was created by Habchy#1665
 # Please join this Discord server if you need help: https://discord.gg/FNNNgqb
