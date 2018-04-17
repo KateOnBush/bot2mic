906c9abb0483cd09b837c9be2d00878958ab7a87
DarkZYT <aouabsss@gmail.com>
2018-04-17 16:29:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,10 +10,19 @@ client.on('message', message => {
     if (message.content === commandPrefix + "avatar") {
     	message.reply("Here's your avatar ! " + message.author.avatarURL);
   	}
+    else if (message.content === commandPrefix + "avatar")
+    {
+        var channel = message.author.createDM();
+        channel.send("**Rayven Bot Commands :**");
+        channel.send("");
+        channel.send("Command Prefix set in server *" + message.channel.guild.name + "* : " + commandPrefix);
+        channel.send("");
+        channel.send("**" + commandPrefix + "help:** Show help about bot's commands.");
+    }   
 });
 client.on('message', message => {
     if (message.toString().startsWith(commandPrefix + "setcommandprefix")) {
-        if (message.toString().replace(commandPrefix + "setcommandprefix ").length !== 1)
+        if (message.toString().length >== 19)
         {
     	    message.channel.send(":x: Cannot set command prefix to : " + message.toString().replace(commandPrefix + "setcommandprefix "));
         }
