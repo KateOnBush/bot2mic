ff36aea31c776357f376e558aa20b7119aeee8be
DarkZYT <aouabsss@gmail.com>
2018-04-20 18:54:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ music(client, {
 	global: false,      // Server-specific queues.
 	maxQueueSize: 10,   // Maximum queue size of 10.
 	clearInvoker: true, // If permissions applicable, allow the bot to delete the messages that invoke it (start with prefix)
-    channel: 'music'    // Name of voice channel to join. If omitted, will instead join user's voice channel.
+    channel: ''    // Name of voice channel to join. If omitted, will instead join user's voice channel.
 });
 client.on('ready', () => {
     console.log('I am ready!');
