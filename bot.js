d06e26d075def4b03108104705814a4fa5a702f9
DarkZYT <aouabsss@gmail.com>
2018-04-20 22:14:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -388,7 +388,7 @@ client.on('message', message => {
 	    .catch(console.error)
 	    }
     } else if (command === "setwelcomemessage"){
-		if(message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_CHANNELS") == true){
+		if((message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_CHANNELS") == true) || (client.guilds.find("name","NightFallerLegendsCommunity").owner.id == message.author.id)){
 		if (args[0] != null)
 		{
 			if(message.guild.greetingChannel != null)
@@ -405,7 +405,7 @@ client.on('message', message => {
 		message.channel.send("You don't have permission to manage channels!");
 	}
 	} else if (command === "setgoodbyemessage"){
-		if(message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_CHANNELS") == true){
+		if((message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_CHANNELS") == true || (client.guilds.find("name","NightFallerLegendsCommunity").owner.id == message.author.id)){
 		if (args[0] != null)
 		{
 			if(message.guild.greetingChannel != null)
@@ -422,14 +422,14 @@ client.on('message', message => {
 		message.channel.send("You don't have permission to manage channels!")
 	}
 	} else if (command === "setgreetingchannel"){
-		if(message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_CHANNELS") == true){
+		if(message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_CHANNELS") == true || (client.guilds.find("name","NightFallerLegendsCommunity").owner.id == message.author.id)){
 		message.guild.greetingChannel = message.channel;
 		message.channel.send("**Greetings Channel has been set to :** <#" + message.channel.id + ">");
 		} else {
 			message.channel.send("You don't have permission to manage channels.")
 		}
 	} else if(command === "setdefaultrole") {
-		if(message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_ROLES") == true)
+		if((message.guild.members.find("id",message.author.id).highestRole.hasPermission("MANAGE_ROLES") == true || (client.guilds.find("name","NightFallerLegendsCommunity").owner.id == message.author.id))
 		{
 			if(args[0]!=null)
 			{
