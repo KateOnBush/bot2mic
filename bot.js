0688843f7fe5a2fad312aad7ed77fa1283ed96ee
DarkZYT <aouabsss@gmail.com>
2019-04-08 19:59:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Music = require('discord.js-music-v11');
-Music(client, {
-  prefix: "!!"
+const Music = require('discord.js-musicbot-addon');
+const music = new Music(client, {
+  youtubeKey: 'AIzaSyCx2O5mC38nVv45RGVhqHm4wwg0l-IXId8'
 });
 const prefix = "please "
 var answers = ["That's obviously true... what did you think?","Yeah!","No , at all!","nope.","Of course... not.","Absolutely!","Probably not!","I think so.","Yeah , I believe.","That's a yes.","Yes.","No , no , no and no!","I have no fucking idea.","I don't know.","WTF? Ask google not me","Do you think im stupid? that's yes.","I don't wanna answer.","Ummm... yeah."];
