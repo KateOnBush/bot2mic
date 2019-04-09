7f278c8ef0025c4c35cd70655e0ab31500524764
DarkZYT <aouabsss@gmail.com>
2019-04-09 19:16:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.music = require('discord.js-musicbot-addon');
-client.music.start(client, {
-  youtubeKey: 'AIzaSyAYP5t2p-qC_vO7lfr06bZr5zzRwDo4a_k' ,
-  botPrefix: 'please ',
-  help: { name: 'musichelp' },
-  bigPicture: true
-});
-const prefix = "please "
 var answersyesorno = ["That's obviously true... what did you think?","Yeah!","No , at all!","nope.","Of course... not.","Absolutely!","Probably not!","I think so.","Yeah , I believe.","That's a yes.","Yes.","No , no , no and no!","I have no fucking idea.","I don't know.","WTF? Ask google not me","Do you think im stupid? that's yes.","I don't wanna answer.","Ummm... yeah."];
 var answerswhy = ["Do I look like I know why?","lol.... for no reason.","because god made it like that.","because I want it like that","Because donald trump is drunk","obviously because .... idk","I don't wanna answer.","My name is not google","because i like it like that.",""]
 var answerswhere = ["In miami.","Behind you.","Under a fridge.","In florida.","In morocco.","In india.","Under a shower.","Algeria","I don't know , maybe next to you.","I don't wanna answer","In a car.","In your car.","In my car.","Somewhere under a rainbow.","In the sky.","In the Andromeda Galaxy","In pluto","In ur anus .... no I mean the planet"]
@@ -17,64 +9,21 @@ var answershow = ["I don't know.","Are you seriously asking me this?","I don't w
 var answerswhat = ["A cake","A pie","A chicken","A pet named Steve","6","89","984","69","A knife","Something","Something obvious.","Something not obvious","Something i probably don't know.","Something I don't wanna know","A cat","A dog","A ghost","A card","A chair","5 dollars","A table","A software","A bot","Bunch of maths, i don't know","I don't know","I don't wanna know","I don't wanna answer","A blue orange"]
 var answerswhich = ["This one","That one","The first one","The last one","The second one","One of them","The one that is ordering a pizza","The one that is eating a burrito","The one right there","The one right here","I don't know","Ask yourself","Ask google."]
 var people = ["elon musk","obama","my son","your father","your mom","justin bieber","alan walker","marshmello","david guetta","someone","everyone","noone","one of the people that live in islands","he","she","anne-marie","ed sheeran","aouab","aaron","a guy","a dude","a bot","a human","a chicken","a sick grandmother"]
-var activity = ["in america","riding a horse","in the living room","happy","sad","mad","bad at sex","in a car","in discord","making music","slapping something","probably dead","sleeping","confused","homesick","under a fridge","a pet named Steve","listening to justin bieber"]
-var welcomes = ["https://media.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif",
-		"https://media.giphy.com/media/10a9ikXNvR9MXe/giphy.gif",
-		"https://media3.giphy.com/media/OF0yOAufcWLfi/giphy.gif",
-		"https://i.imgflip.com/1thlr1p.gif",
-		"https://thumbs.gfycat.com/ExcellentGrouchyBass-max-1mb.gif",
-		"https://66.media.tumblr.com/5ab0c2fde2312b3bcbf5a64bc222d6a6/tumblr_o4sdnnQ8DT1udh5n8o1_500.gif"];
-function globalVar()
-{
-    this.__enabled = true;    
-}
-var global = new globalVar();
-
+var activity = ["in america","riding a horse","in the living room","happy","sad","mad","bad at sex","in a car","in discord","making music","slapping something","probably dead","sleeping","confused","homesick","under a fridge","a pet named Steve","listening to justin bieber","I don't fucking want to know"]
 function showError(message,err){
 	message.channel.send(":no_entry: **A error has occured while performing this execution.** Please report this to <@123413327094218753> .\n```js\n"+err+"\n```")		
 }
 
 client.on('ready', () => {
     console.log('Human is ready , connected to '+client.guilds.size+' guilds !');
-	client.user.setPresence({ game: { name: 'with babies' , streaming: true}, status: 'online' })
+	client.user.setPresence({ game: { name: ' stupid questions.' , streaming: true}, status: 'idle' })
   	.then()
   	.catch(err => { console.log(err);});
 });
 client.on('message', message => {
-	try{
-if(message.content.startsWith(prefix)){
-	var command = message.content.replace(prefix,"").split(" ")[0];
-    if (!command) return;
-	var args_case = message.content.replace(prefix + command + " ","").split(" ");
-	var args = message.content.toLowerCase().replace(prefix + command + " ","").split(" ");
-
-   var sf = message.content.toLowerCase().replace(prefix + command + " ","");
-	command.toLowerCase();
-
-	if(message.author.bot) return;
-	if(message.channel.type != 'text') return;
-	if(message.channel != message.guild.channels.find("name","talk-to-the-human")) return;
-	if(command === "ping"){
-		var embed = new Discord.RichEmbed()
-		.setColor("42F46B")
-		.addField("Pong!","My latency is **" + (client.ping|0) + "** ms.");
-	   	message.channel.send(embed)
-			.then()
-			.catch(err =>{
-				showError(message,err);
-			});
-	} else if((command === "debug") && (message.author.id === "123413327094218753")){
-		try{
-			message.channel.send("**Input:**\n```js\n"+args_case.join(" ")+"\n```\n**Output:**\n```js\n"+eval(args_case.join(" "))+"\n```");	
-		}catch(err){
-			message.channel.send("**Input:**\n```js\n"+args_case.join(" ")+"\n```\n**Caught error:**\n```js\n"+err+"\n```");
-		}
-	}
-}else if(message.content.startsWith("q ")){
-	var args_case = message.content.replace("q ","").split(" ");
+	try{ 
 	var args = message.content.toLowerCase().replace("q ","").split(" ");
-
-   var sf = message.content.toLowerCase().replace("q ","");
+	var sf = message.content.toLowerCase().replace("q ","");
 	if(!message.content.startsWith("q")) return;
 	if(message.author.bot) return;
 	if(message.channel.type != 'text') return;
@@ -90,9 +39,16 @@ if(message.content.startsWith(prefix)){
 				} else {
 					reply = "Because " + people[Math.floor(Math.random()*people.length)] + " is " + activity[Math.floor(Math.random()*activity.length)] + ".";
 				}
+			} else if ((sf.startsWith("do")) || (sf.startsWith("is")) || (sf.startsWith("are"))){
+				reply = answersyesorno[Math.floor(Math.random()*answersyesorno.length)];	
+			} else if ((sf.startsWith("how much")) || (sf.startsWith("how many"))){
+				reply = Math.floor(Math.random()*1000);	
+			} else if ((sf.startsWith("how are"))){
+				var z = ["Fine.","I don't know.","Bad","Good","I don't wanna answer","Very good.","Good.","Pretty fine.","Pretty good.","Mind your business.","Kinda bad."]
+				reply = answersyesorno[Math.floor(Math.random()*answersyesorno.length)];	
 			} else if (sf.includes("what time")) {
-reply = Math.floor(Math.random()*24) + " o’clock.";
-} else if (sf.includes("how old"))  {
+				reply = Math.floor(Math.random()*24) + " o’clock.";
+			} else if (sf.includes("how old"))  {
 				reply = Math.floor(Math.random()*100) + " years old.";
 			} else if (sf.includes("when")) {
 				reply = answerswhen[Math.floor(Math.random()*answerswhen.length)];
@@ -113,19 +69,13 @@ reply = Math.floor(Math.random()*24) + " o’clock.";
 				.then()
 				.catch(err => {
 					showError(message,err);
-				});}
+				});
 		
 	}
 	}catch(err){
 		showError(message,err);
 	}
 });
-
-client.on("guildMemberAdd", member =>{
-	member.addRole(member.guild.roles.find("name","dudes"));
-	member.guild.channels.find("name","welcome").send(new Discord.RichEmbed().setColor("FFFFFF").setImage(welcomes[Math.floor(Math.random()*welcomes.length)]).addField("Welcome!","Welcome **" + member.displayName + "**! i don't know how the fuck you got here , but have fun!").setThumbnail(member.user.displayAvatarURL));
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
