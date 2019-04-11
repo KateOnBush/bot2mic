a4952f2c260c8d9e29f3cb3f11d325614f972a1f
DarkZYT <aouabsss@gmail.com>
2019-04-11 13:11:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var answersyesorno = ["That's obviously true... what did you think?","Yeah!","No , at all!","nope.","Of course... not.","Absolutely!","Probably not!","I think so.","Yeah , I believe.","That's a yes.","Yes.","No , no , no and no!","I have no fucking idea.","I don't know.","WTF? Ask google not me","Do you think im stupid? that's yes.","I don't wanna answer.","Ummm... yeah."];
+var answersyesorno = ["yup","nopeeeee","no lol","no lmao","nop","yay","ofc","Of course","no","i guess so","i don't think so","That's obviously true... what did you think?","Yeah!","No , at all!","nope.","Of course... not.","Absolutely!","Probably not!","I think so.","Yeah , I believe.","That's a yes.","Yes.","No , no , no and no!","I have no fucking idea.","I don't know.","WTF? Ask google not me","Do you think im stupid? that's yes.","I don't wanna answer.","Ummm... yeah."];
 var answerswhy = ["Do I look like I know why?","lol.... for no reason.","because god made it like that.","because I want it like that","Because donald trump is drunk","obviously because .... idk","I don't wanna answer.","My name is not google","because i like it like that.",""]
 var answerswhere = ["In miami.","Behind you.","Under a fridge.","In florida.","In morocco.","In india.","Under a shower.","Algeria","I don't know , maybe next to you.","I don't wanna answer","In a car.","In your car.","In my car.","Somewhere under a rainbow.","In the sky.","In the Andromeda Galaxy","In pluto","In ur anus .... no I mean the planet"]
 var answerswho = ["Elon Musk","Donald trump","Your mom.","Nick","Someone","Your dad","Your dad's mom","Obama","Aaron","Aouab","A pet named Steve","A russian girl living in Italy","An irish dog in a british hospital"]
@@ -9,8 +9,8 @@ var answershow = ["I don't know.","Are you seriously asking me this?","I don't w
 var answerswhat = ["A cake","A pie","A chicken","A pet named Steve","6","89","984","69","A knife","Something","Something obvious.","Something not obvious","Something i probably don't know.","Something I don't wanna know","A cat","A dog","A ghost","A card","A chair","5 dollars","A table","A software","A bot","Bunch of maths, i don't know","I don't know","I don't wanna know","I don't wanna answer","A blue orange"]
 var answerswhich = ["This one","That one","The first one","The last one","The second one","One of them","The one that is ordering a pizza","The one that is eating a burrito","The one right there","The one right here","I don't know","Ask yourself","Ask google."]
 var people = ["elon musk","obama","my son","your father","your mom","justin bieber","alan walker","marshmello","david guetta","someone","everyone","noone","one of the people that live in islands","he","she","anne-marie","ed sheeran","aouab","aaron","a guy","a dude","a bot","a human","a chicken","a sick grandmother","Carlos","Dave","Samuel","Dojo","Shen","one of Obama's friends"]
-var responses = ["Okay then.","Okay","K.","Really?","Ok.","lol.","lmao","hahahah","for real?","hmmm..","xD.",":Moooore:",":huh:",":evilpatrick:"]
-var nothing = ["Can u repeat ? I didn't hear you.","You didn't say anything '-'","U didn't say anything.","what are u saying :huh:","lol i can't reply to nothing xD",":evilpatrick: :evilpatrick: :evilpatrick: say smtg wtf","are u seriously expecting me to reply to nothing?",":Bullshit: stop writing nothing."]
+var responses = ["Good","Fine","Okay then.","Okay","K.","Really?","Ok.","lol.","lmao","hahahah","for real?","hmmm..","xD.","<:Moooore:427561997605928973>","<:huh:427562025229484033>","<:evilpatrick:427562269195632658>"]
+var nothing = ["Can u repeat ? I didn't hear you.","You didn't say anything '-'","U didn't say anything.","what are u saying <:huh:427562025229484033>","lol i can't reply to nothing xD","<:evilpatrick:427562269195632658><:evilpatrick:427562269195632658><:evilpatrick:427562269195632658> say smtg wtf","are u seriously expecting me to reply to nothing?","<:Bullshit:427561941230419988> stop writing nothing."]
 var activity = ["in america","riding a horse","in the living room","happy","sad","mad","bad at sex","in a car","in discord","making music","slapping something","probably dead","sleeping","confused","homesick","under a fridge","a pet named Steve","listening to justin bieber","depressed","making a pizza","having lunch","in SpaceX","in an apple store","watching Netflix."]
 function showError(message,err){
 	message.channel.send(":no_entry: **A error has occured while performing this execution.** Please report this to <@123413327094218753> .\n```js\n"+err+"\n```")		
@@ -48,7 +48,7 @@ client.on('message', message => {
 				}
 			} else if ((sf.startsWith("do")) || (sf.startsWith("is")) || (sf.startsWith("are"))){
 				reply = answersyesorno[Math.floor(Math.random()*answersyesorno.length)];	
-			} else if ((sf.startsWith("how much")) || (sf.startsWith("how many"))){
+			} else if ((sf.includes("how much")) || (sf.includes("how many"))){
 				reply = Math.floor(Math.random()*1000);	
 			} else if (sf.includes("how are you")){
 				var z = ["Fine.","I don't know.","Bad","Good","I don't wanna answer","Very good.","Good.","Pretty fine.","Pretty good.","Mind your business.","Kinda bad."]
