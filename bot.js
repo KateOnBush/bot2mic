db02fac439aa8fcffab6ebaffcb4c4e419edcd2b
DarkZYT <aouabsss@gmail.com>
2022-10-05 23:04:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds]});
 
-client.on('message', message => {
+const commands = [
+	new SlashCommandBuilder().setName('salut').setDescription('Bonsoir!')
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async ()=>{
+
+	try {
+		
+	await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+		
+	} catch(err){
 	
-	message.channel.send("hi haha");
-	
-});
+		console.log("Erreur de sa mere " + error);
+		
+	}
+
+})();
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
