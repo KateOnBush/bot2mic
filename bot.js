21233961aaf49ca28578c90840b35606ee5de8ba
DarkZYT <aouabsss@gmail.com>
2022-10-05 22:54:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Human is ready , connected to '+client.guilds.size+' guilds !');
-	client.user.setPresence({ game: { name: ' stupid questions.' , streaming: true}, status: 'idle' })
-  	.then()
-  	.catch(err => { console.log(err);});
-});
 client.on('message', message => {
 	
 	
 });
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
