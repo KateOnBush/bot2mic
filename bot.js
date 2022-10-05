255e2efacfd2f3b84733b0068c4e3a47504133e9
DarkZYT <aouabsss@gmail.com>
2022-10-05 23:00:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({});
+const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds]});
 
 client.on('message', message => {
 	
+	message.channel.send("hi haha");
 	
 });
 
