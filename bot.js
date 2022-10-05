23554bc9798b33dd4bc243155950b85a31924dae
DarkZYT <aouabsss@gmail.com>
2022-10-05 22:44:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-	//Knowledge
-	var knowledge = {
-		greetings: ["hey","yo","hello","hi","wassup","what's up","whatsup","hola","greetings"],
-		wishes: ["good morning","good evening","good night","good afternoon"],
-		verbs: {
-			defining: ["is","are","am"]
-		}
-	};
-
-	//Replies
-	var replies = {
-		greetings: ["Hey there","Hey","Hi","Oh hey","What's up","Yo"]
-	};
-
-
-function showError(message,err){
-	message.channel.send(":no_entry: **A error has occured while performing this execution.** Please report this to <@123413327094218753> .\n```js\n"+err+"\n```")		
-}
-
 client.on('ready', () => {
     console.log('Human is ready , connected to '+client.guilds.size+' guilds !');
 	client.user.setPresence({ game: { name: ' stupid questions.' , streaming: true}, status: 'idle' })
@@ -27,20 +8,6 @@ client.on('ready', () => {
   	.catch(err => { console.log(err);});
 });
 client.on('message', message => {
-	
-	if (message.author.bot) return;
-	
-	var input = message.content.toLowerCase();
-	var kindOfMessage=0; // 1: greeting
-	knowledge.greetings.forEach(g=>{
-		if(input.startsWith(g) && kindOfMessage==0){
-			kindOfMessage=1;
-			console.log("it's a greeting!");
-		}
-	});
-	if (kindOfMessage==1){
-		message.channel.send(replies.greetings[Math.round(Math.random()*(replies.greetings.length-1))] + "!");
-	}
 	
 	
 });
