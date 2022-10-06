0aa8b2a32e5dc3a365033af9f1d0ffda744235a0
DarkZYT <aouabsss@gmail.com>
2022-10-06 17:29:16 +0200

    yes

--- bot.js
+++ bot.js
@@ -34,21 +34,21 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 })();
 
+
+
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-  
+
 	if (interaction.commandName === 'groupe') {
-		if (interaction.commandName === 'button') {
-			const row = new ActionRowBuilder()
-				.addComponents(
-					new ButtonBuilder()
-						.setCustomId('primary')
-						.setLabel('MIC A!')
-						.setStyle(ButtonStyle.Primary),
-				);
-	
-			await interaction.reply({ content: 'Choisi ton groupe mon poto!', components: [row] });
-		}
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('primary')
+					.setLabel('MIC A!')
+					.setStyle(ButtonStyle.Primary),
+			);
+
+		await interaction.reply({ content: 'Choisi ton groupe mon poto!', components: [row] });
 	}
   });
 
