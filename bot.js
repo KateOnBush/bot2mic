4f700de3462aab0a6eea633b35abcdd8888ce6dc
DarkZYT <aouabsss@gmail.com>
2022-10-06 18:03:05 +0200

    s

--- bot.js
+++ bot.js
@@ -13,7 +13,8 @@ const {
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
 const commands = [
-	new SlashCommandBuilder().setName('groupe').setDescription('Choisi ton groupe 2MIC!')
+	new SlashCommandBuilder().setName('groupe').setDescription('Choisi ton groupe 2MIC!'),
+
 ]
 	.map(command => command.toJSON());
 
@@ -37,26 +38,62 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 })();
 
 
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+///Chat Input Interaction
+async function processChatInteraction(interaction){
 
 	if (interaction.commandName === 'groupe') {
 		const row = new ActionRowBuilder()
 			.addComponents(
 				new ButtonBuilder()
-					.setCustomId('mica')
-					.setLabel('MIC A!')
+					.setCustomId('grp_mica')
+					.setLabel('MIC A')
 					.setStyle(ButtonStyle.Secondary),
 				new ButtonBuilder()
-					.setCustomId('micb')
-					.setLabel('MIC B!')
-					.setStyle(ButtonStyle.Secondary)
+					.setCustomId('grp_micb')
+					.setLabel('MIC B')
+					.setStyle(ButtonStyle.Secondary),
+				new ButtonBuilder()
+					.setCustomId('grp_micc')
+					.setLabel('MIC C')
+					.setStyle(ButtonStyle.Secondary),
+				new ButtonBuilder()
+					.setCustomId('grp_micd')
+					.setLabel('MIC D')
+					.setStyle(ButtonStyle.Secondary),
+				new ButtonBuilder()
+					.setCustomId('grp_fasnum')
+					.setLabel('Fas. Num.')
+					.setStyle(ButtonStyle.Danger)
 			);
-
 
 		await interaction.reply({ content: 'Choisi ton groupe mon poto!', components: [row] });
 	}
+
+}
+
+
+async function processButtonInteraction(interaction){
+
+	if(interaction.id.startsWith("grp_")){
+
+		interaction.reply({content: "Bah oui :)", ephemeral: true});
+
+	}
+
+}
+
+
+
+///Register all interactions :)
+client.on('interactionCreate', async interaction => {
+
+	if (interaction.isChatInputCommand()) await processChatInteraction(interaction);
+
+	if (interaction.isButton()) await processButtonInteraction(interaction);
+
+	return;
+
+
   });
 
 // THIS  MUST  BE  THIS  WAY
